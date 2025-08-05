a562a18f1c9e34675f483362bac1ce39a0ef3d7d
IChilling <88207542+IChilling@users.noreply.github.com>
2025-08-05 16:58:12 -0700

    Update AutoStartMutationMachine.lua

--- AutoStartMutationMachine.lua
+++ AutoStartMutationMachine.lua
@@ -1,5 +1,7 @@
 -- This Script Automatically Starts The Mutation Machine
 
+-- Allows you to start the Mutation Machine early, so then later on you can simply put a pet in and it will be ready to collect, as long as an hour has passed
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local GameEvents = ReplicatedStorage:WaitForChild("GameEvents")
 local PetMutationMachineService_RE = GameEvents:WaitForChild("PetMutationMachineService_RE")
