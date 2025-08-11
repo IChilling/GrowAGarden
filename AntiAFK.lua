b6a0e1b3d70bfc4b733bb247ceb73e7eb7e0551e
IChilling <88207542+IChilling@users.noreply.github.com>
2025-08-11 06:25:15 -0700

    Create AntiAFK.lua

---
+++ AntiAFK.lua
@@ -0,0 +1,22 @@
+if getgenv().ChillingAntiAFK then
+  warn("Chilling's AntiAFK is already running")
+  return
+end
+
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local VirtualUser = game:GetService("VirtualUser")
+local Players = game:GetService("Players")
+local lp = Players.LocalPlayer
+
+lp.Idled:Connect(function()
+    VirtualUser:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    VirtualUser:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    print("[Chilling's AntiAFK]: AFK Kick Prevented")
+end)
+
+getgenv().ChillingAntiAFK = true
+print("Started Chilling's AntiAFK")
